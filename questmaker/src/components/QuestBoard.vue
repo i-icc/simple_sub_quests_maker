d4bb7f92b4f51a1900577b68ec6b66c98f439431
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2023-02-23 23:10:19 +0900

    okay

--- questmaker/src/components/QuestBoard.vue
+++ questmaker/src/components/QuestBoard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="board">
-    <span>{{ quest }}</span>
+    <span class="und">Quest</span>
+    <div class="quest">{{ quest }}</div>
   </div>
 </template>
 
@@ -15,5 +16,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.board {
+  border: 3px dashed;
+  margin: 3vw;
+  padding: 2vw;
+}
+.und {
+  border-bottom: solid 1px gray;
+  font-size: 8vw;
+}
+.quest {
+  font-size: 5vw;
+  margin-top: 3vw;
+}
 </style>
--- questmaker/src/views/AboutView.vue
+++ questmaker/src/views/AboutView.vue
@@ -15,6 +15,7 @@
 <style scoped>
 .about {
   margin: 3rem;
+  font-size: 3vw;
 }
 
 .und {
